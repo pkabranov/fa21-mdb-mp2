1a4f9de1171ba1dcf59458fd23c3816d63c481b9
Philip Kabranov <Kabranov12@gmail.com>
2021-10-03 15:43:34 -0700

    basic stack

--- App.js
+++ App.js
@@ -21,6 +21,24 @@ import MovieFilterScreen from "./screens/MovieFilterScreen/MovieFilterScreen.mai
   Read the example carefully to set up this app's screen heirarchy in a similar manner.
 
   https://reactnavigation.org/docs/modal */
+
+const Stack = createStackNavigator();
+
 export default function App() {
-  return <NavigationContainer>{}</NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='MovieDetailScreen'>
+        <Stack.Screen
+          name="MovieListScreen"
+          component={MovieListScreen}
+          options={{ title: "MovieListScreen" }}
+        />
+        <Stack.Screen
+          name="MovieDetailScreen"
+          component={MovieDetailScreen}
+          options={{ title: "MovieDetailScreen" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- screens/MovieDetailScreen/MovieDetailScreen.main.jsx
+++ screens/MovieDetailScreen/MovieDetailScreen.main.jsx
@@ -7,9 +7,20 @@ export default function MovieDetailScreen({ route }) {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.container}>
-        {/* TODO: Configure this screen to have an image and appropriate text describing the movie. 
+        {
+          /* TODO: Configure this screen to have an image and appropriate text describing the movie. 
                 See the example on the spec for design inspiration.
-                Feel free to use the styles below. */}
+                Feel free to use the styles below. */
+          <Text style={styles.text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+        }
       </ScrollView>
     </SafeAreaView>
   );
